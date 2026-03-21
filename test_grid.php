45ba9b4a474a33b86f0b87e67ab550c1ddafcf67
Ariel Martinez <ariel.martinez@advisorsy.es>
2026-03-21 23:34:47 +0100

    sentry

--- test_grid.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "--------- CNAE MAIN --------- \n";
-preg_match('/<main>(.*?)<\/main>/s', file_get_contents('app/Views/seo/cnae.php'), $m);
-echo substr($m[1], 0, 500) . "\n...\n";
-
-echo "--------- NEW PROVINCE MAIN --------- \n";
-preg_match('/<main>(.*?)<\/main>/s', file_get_contents('app/Views/seo/new_province.php'), $m2);
-echo substr($m2[1], 0, 500) . "\n...\n";
