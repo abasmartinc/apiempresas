72cd453384606f86b1d2babf822626bc7922a300
Ariel <ariel.martinez@advisorsy.es>
2026-06-19 16:20:26 +0200

    ficha de empresa

--- app/Models/BormePostsModel.php
+++ app/Models/BormePostsModel.php
@@ -19,6 +19,27 @@ class BormePostsModel extends Model
     protected $returnType = 'array';
     protected $useTimestamps = false; 
 
+    // Eventos del modelo
+    protected $afterInsert = ['invalidateBormeSummary'];
+
+    /**
+     * Invalida el resumen del BORME generado por IA cuando entra un acto nuevo
+     */
+    protected function invalidateBormeSummary(array $data)
+    {
+        if (isset($data['data']['company_id'])) {
+            $companyId = (int)$data['data']['company_id'];
+            $db = \Config\Database::connect();
+            
+            // Ponemos a NULL el resumen para que el Cron (seo:enrich-borme) lo regenere
+            $db->table('company_enrichment')
+               ->where('company_id', $companyId)
+               ->update(['ai_borme_summary' => null]);
+        }
+        
+        return $data;
+    } 
+
     /**
      * Get posts by company ID
      */
