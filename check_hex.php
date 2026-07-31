ec5eb49a1fea82e33e45527ee7b9d204cbac8d25
Ariel <ariel.martinez@advisorsy.es>
2026-07-31 16:06:00 +0200

    google sheets

--- check_hex.php
+++
@@ -1,3 +0,0 @@
-<?php
-$c = file_get_contents('app/Views/home.php');
-echo "Hex dump: " . bin2hex(substr($c, 0, 10)) . "\n";
--- query_prod2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli('217.61.210.127', 'apiempresas_user', 'WONwyjpsmx3h3$@2', 'reseller3537_apiempresas');
-if ($mysqli->connect_error) { die('Connect Error: ' . $mysqli->connect_error); }
-
-$res = $mysqli->query("SELECT cta_label, COUNT(*) as clicks FROM radar_demo_events WHERE event_type LIKE '%coming_soon%' GROUP BY cta_label ORDER BY clicks DESC");
-echo "RESULTS:\n";
-while ($row = $res->fetch_assoc()) {
-    echo $row['cta_label'] . ' | ' . $row['clicks'] . "\n";
-}
