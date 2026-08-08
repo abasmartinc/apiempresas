12f982029a01659457bafad3fe78b962fe372e61
Ariel <ariel.martinez@advisorsy.es>
2026-08-08 10:08:19 +0200

    holdings

--- desc_tracking.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new mysqli('217.61.210.127', 'apiempresas_user', 'WONwyjpsmx3h3$@2', 'reseller3537_apiempresas');
-if($db->connect_error) die('Connection failed');
-
-$res = $db->query("DESCRIBE tracking_events");
-while($row = $res->fetch_assoc()) {
-    print_r($row);
-}
