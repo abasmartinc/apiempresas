3193a81f47913a4c046a0c6c23289d4e24f8163f
Ariel <ariel.martinez@advisorsy.es>
2026-08-21 21:32:26 +0200

    score

--- scratch_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
-chdir(__DIR__);
-require 'public/index.php';
-$db = \Config\Database::connect();
-$row = $db->table('company_risk_profiles')->where('risk_score', 15)->get()->getRowArray();
-print_r($row);
